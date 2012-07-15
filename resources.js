3710960c6d40be50908331dabbe9fbb4d9cd6681
Jackson Tian <shyvo1987@gmail.com>
2012-07-16 02:01:01 +0800

    Add bootstrap and track code.

--- resources.js
+++ resources.js
@@ -1,7 +1,43 @@
 var Localize = require('localize');
 module.exports = new Localize({
+  "Hujs": {
+    "en-us": "Hu.js",
+    "zh-cn": "沪js"
+  },
   "Hu.js coming soon!": {
     "en-us": "Hu.js coming soon!",
     "zh-cn": "沪js即将到来！"
+  },
+  "Homepage": {
+    "en-us": "Home",
+    "zh-cn": "主页"
+  },
+  "Schedule": {
+    "en-us": "Schedule",
+    "zh-cn": "日程安排"
+  },
+  "Venue": {
+    "en-us": "Venue",
+    "zh-cn": "会议场地"
+  },
+  "Speakers": {
+    "en-us": "Speakers",
+    "zh-cn": "演讲嘉宾"
+  },
+  "Sponsors": {
+    "en-us": "Sponsors",
+    "zh-cn": "赞助商"
+  },
+  "Community": {
+    "en-us": "Community",
+    "zh-cn": "社区交流"
+  },
+  "About": {
+    "en-us": "About",
+    "zh-cn": "关于我们"
+  },
+  "Contact": {
+    "en-us": "Contact",
+    "zh-cn": "联系方式"
   }
 });
