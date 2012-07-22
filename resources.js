174d09ae876d28c9985110f02f0758f020cf9604
Jackson Tian <shyvo1987@gmail.com>
2012-07-22 21:56:11 +0800

    Update.

--- resources.js
+++ resources.js
@@ -93,5 +93,25 @@ exports.index = {
   "More topics": {
     "en-us": "More topics",
     "zh-cn": "我还想听"
+  },
+  "I wanna listen it": {
+    "en-us": "I wanna listen it +1",
+    "zh-cn": "我想听+1"
+  },
+  "Not enough": {
+    "en-us": "If above topics aren't make you satisfaction, could you submit your purpose topic or speaker, we will try ours best to invite the topic&amp;speaker for you",
+    "zh-cn": "上面的话题都不感兴趣，还不够给力？遗漏了您喜欢的大牛没有邀请？您还有更好的推荐或是自荐，无论国内还是国外的嘉宾？速填表单，组委会会为您尽力邀请。"
+  },
+  "I wanna listen too!": {
+    "en-us": "I wanna listen too!",
+    "zh-cn": "我还想听"
+  },
+  "eg. topic": {
+    "en-us": "eg. Connect/Express",
+    "zh-ch": "比如：JSCEX"
+  },
+  "eg. speaker": {
+    "en-us": "eg. TJ",
+    "zh-ch": "比如：老赵"
   }
 };
